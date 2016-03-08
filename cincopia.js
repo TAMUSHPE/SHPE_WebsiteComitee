5b9cb78ca6a87e3d3d3b6e388553df6512b68d42
bote795 <nbotell2@gmail.com>
2016-03-07 23:35:43 -0600

    regex to get key features

---
+++ cincopia.js
@@ -0,0 +1,9 @@
+//regex to get id
+//	 id="(.*)">. 
+//object  
+//	_object\"]\s="(.*)"; 
+//	
+//	regex to get small key
+//		cpo\["_fid"] = "(.*)";
+//	regex for span
+//		solution.(.*)<\/noscript>
